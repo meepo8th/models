d050b4685bbc9a84c5a1cf4fbc272a63646b00a3
meepo8th <94739473@qq.com>
2017-06-27 14:17:40 +0800

    增加自动标注

--- object_detection/create_pascal_tf_record.py
+++ object_detection/create_pascal_tf_record.py
@@ -16,8 +16,8 @@
 r"""Convert raw PASCAL dataset to TFRecord for object_detection.
 
 Example usage:
-    ./create_pascal_tf_record --data_dir=/home/user/VOCdevkit \
-        --year=VOC2012 \
+    ./create_pascal_tf_record --data_dir=/home/user/VOCdevkit /
+        --year=VOC2012 /
         --output_path=/home/user/pascal.record
 """
 from __future__ import absolute_import
@@ -38,13 +38,13 @@ from object_detection.utils import label_map_util
 
 
 flags = tf.app.flags
-flags.DEFINE_string('data_dir', '', 'Root directory to raw PASCAL VOC dataset.')
-flags.DEFINE_string('set', 'train', 'Convert training set, validation set or '
+flags.DEFINE_string('data_dir', 'E:/picRecord/data/data', 'Root directory to raw PASCAL VOC dataset.')
+flags.DEFINE_string('set', 'test', 'Convert training set, validation set or '
                     'merged set.')
 flags.DEFINE_string('annotations_dir', 'Annotations',
                     '(Relative) path to annotations directory.')
-flags.DEFINE_string('year', 'VOC2007', 'Desired challenge year.')
-flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string('year', 'VOC2012', 'Desired challenge year.')
+flags.DEFINE_string('output_path', 'E:/picRecord/data/record/pascal_test.record', 'Path to output TFRecord')
 flags.DEFINE_string('label_map_path', 'data/pascal_label_map.pbtxt',
                     'Path to label map proto')
 flags.DEFINE_boolean('ignore_difficult_instances', False, 'Whether to ignore '
@@ -81,8 +81,9 @@ def dict_to_tf_example(data,
   Raises:
     ValueError: if the image pointed to by data['filename'] is not a valid JPEG
   """
-  img_path = os.path.join(data['folder'], image_subdirectory, data['filename'])
+  img_path = os.path.join(data['folder'], image_subdirectory, data['filename']+".jpg")
   full_path = os.path.join(dataset_directory, img_path)
+  print(full_path)
   with tf.gfile.GFile(full_path, 'rb') as fid:
     encoded_jpg = fid.read()
   encoded_jpg_io = io.BytesIO(encoded_jpg)
@@ -103,43 +104,45 @@ def dict_to_tf_example(data,
   truncated = []
   poses = []
   difficult_obj = []
-  for obj in data['object']:
-    difficult = bool(int(obj['difficult']))
-    if ignore_difficult_instances and difficult:
-      continue
+  if 'object' in data:
+      for obj in data['object']:
+        difficult = bool(int(obj['difficult']))
+        if ignore_difficult_instances and difficult:
+          continue
 
-    difficult_obj.append(int(difficult))
+        difficult_obj.append(int(difficult))
 
-    xmin.append(float(obj['bndbox']['xmin']) / width)
-    ymin.append(float(obj['bndbox']['ymin']) / height)
-    xmax.append(float(obj['bndbox']['xmax']) / width)
-    ymax.append(float(obj['bndbox']['ymax']) / height)
-    classes_text.append(obj['name'].encode('utf8'))
-    classes.append(label_map_dict[obj['name']])
-    truncated.append(int(obj['truncated']))
-    poses.append(obj['pose'].encode('utf8'))
+        xmin.append(float(obj['bndbox']['xmin']) / width)
+        ymin.append(float(obj['bndbox']['ymin']) / height)
+        xmax.append(float(obj['bndbox']['xmax']) / width)
+        ymax.append(float(obj['bndbox']['ymax']) / height)
+        classes_text.append(obj['name'].encode('utf8'))
+        classes.append(label_map_dict[obj['name']])
+        truncated.append(int(obj['truncated']))
+        poses.append(obj['pose'].encode('utf8'))
 
-  example = tf.train.Example(features=tf.train.Features(feature={
-      'image/height': dataset_util.int64_feature(height),
-      'image/width': dataset_util.int64_feature(width),
-      'image/filename': dataset_util.bytes_feature(
-          data['filename'].encode('utf8')),
-      'image/source_id': dataset_util.bytes_feature(
-          data['filename'].encode('utf8')),
-      'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
-      'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-      'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
-      'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-      'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-      'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-      'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-      'image/object/class/label': dataset_util.int64_list_feature(classes),
-      'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
-      'image/object/truncated': dataset_util.int64_list_feature(truncated),
-      'image/object/view': dataset_util.bytes_list_feature(poses),
-  }))
-  return example
+      example = tf.train.Example(features=tf.train.Features(feature={
+          'image/height': dataset_util.int64_feature(height),
+          'image/width': dataset_util.int64_feature(width),
+          'image/filename': dataset_util.bytes_feature(
+              data['filename'].encode('utf8')),
+          'image/source_id': dataset_util.bytes_feature(
+              data['filename'].encode('utf8')),
+          'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+          'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+          'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+          'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+          'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+          'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+          'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+          'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+          'image/object/class/label': dataset_util.int64_list_feature(classes),
+          'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+          'image/object/truncated': dataset_util.int64_list_feature(truncated),
+          'image/object/view': dataset_util.bytes_list_feature(poses),
+      }))
+      return example
+  return None
 
 
 def main(_):
@@ -160,7 +163,7 @@ def main(_):
   for year in years:
     logging.info('Reading from PASCAL %s dataset.', year)
     examples_path = os.path.join(data_dir, year, 'ImageSets', 'Main',
-                                 'aeroplane_' + FLAGS.set + '.txt')
+                                 '' + FLAGS.set + '.txt')
     annotations_dir = os.path.join(data_dir, year, FLAGS.annotations_dir)
     examples_list = dataset_util.read_examples_list(examples_path)
     for idx, example in enumerate(examples_list):
@@ -174,7 +177,8 @@ def main(_):
 
       tf_example = dict_to_tf_example(data, FLAGS.data_dir, label_map_dict,
                                       FLAGS.ignore_difficult_instances)
-      writer.write(tf_example.SerializeToString())
+      if not tf_example == None:
+        writer.write(tf_example.SerializeToString())
 
   writer.close()
 
