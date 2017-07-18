136f73c6fca26dbc6c283907a3b2d3207fdc83b2
woshi <111>
2017-07-18 09:42:09 +0800

    调整检测算法

--- object_detection/demo/demo.py
+++ object_detection/demo/demo.py
@@ -1,9 +1,11 @@
 import os.path
-
+import traceback
 from qtpy import QtGui
 
 from object_detection.demo.lib.lib import struct, newAction, newIcon, addActions, fmtShortcut, downLoadPic
 import sys
+from object_detection.demo.lib.canvas import Canvas
+from object_detection.demo.lib.shape import Shape
 
 try:
     from PyQt5.QtGui import *
@@ -39,34 +41,78 @@ def get_main_app(argv=[]):
 class MainWindow(QMainWindow):
     def open(self):
         picPath = QFileDialog.getOpenFileName(self, "open file dialog", "C:",
-                                              "picture files(*.png,*.jpg,*.bmp)")
-        self.openWithPath(picPath)
+                                              "picture files(*.png;*.jpg;*.bmp)")
+        self.openWithPath(picPath[0])
+
+    def scrollRequest(self, delta, orientation):
+        units = - delta / (8 * 15)
+        bar = self.scrollBars[orientation]
+        bar.setValue(bar.value() + bar.singleStep() * units)
 
     def openWithPath(self, picPath):
-        data = libTensor.detect(picPath)
-        if not data.keys().__len__ == 0:
-            print(data)
-        else:
-            print("no object")
+        self.showTips(u"图片识别中...")
+        infoData = (0, 0)
+        try:
+            self.showPic(picPath)
+            data = libTensor.detect(picPath)
+            if data:
+                self.loadData(data, picPath)
+                infoData = libTensor.countPersonWithElement(data)
+        except Exception:
+            self.showTips(u"图片识别失败")
+            print(traceback.format_exc())
+        msg = u"识别到%s个人，其中%s个人没有佩戴安全帽" % (infoData[0], infoData[1])
+        self.showTips(msg)
+        print(msg)
+
+    def loadData(self, data, picPath):
+        image = QImage()
+        image.load(picPath)
+        width = image.width()
+        imageScaled = image.scaled(self.canvas.width(), self.canvas.height(), Qt.KeepAspectRatio)
+        scaleWidth = imageScaled.width()
+        scale = (scaleWidth + 0.0) / width
+        s = []
+        if data:
+            for key, values in data.items():
+                for value in values:
+                    shape = Shape(label=key)
+                    shape.addPoint(QPointF(scale * value[0], scale * value[1]))
+                    shape.addPoint(QPointF(scale * value[0], scale * value[3]))
+                    shape.addPoint(QPointF(scale * value[2], scale * value[3]))
+                    shape.addPoint(QPointF(scale * value[2], scale * value[1]))
+                    shape.close()
+                    s.append(shape)
+            self.canvas.loadShapes(s)
+
+    def showPic(self, picPath):
+        image = QImage()
+        if image.load(picPath):
+            image = image.scaled(self.canvas.width(), self.canvas.height(), Qt.KeepAspectRatio)
+            self.canvas.loadPixmap(QPixmap.fromImage(image))
+
+    def showTips(self, tips):
+        self.statusBar().showMessage(tips)
+        pass
 
     def goWithUrl(self, url):
         picPath = downLoadPic(url)
         if '' == picPath:
-            msg_box = QMessageBox(QMessageBox.Warning, u"提示", u"输入路径不正确")
-            msg_box.exec_()
+            self.showTips(u"获取图片失败")
         else:
             self.openWithPath(picPath)
 
     def go(self):
+        self.showTips(u"获取图片中...")
         self.goWithUrl(self.url.text())
 
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle(__appname__)
         self.toolbar = self.addToolBar("toolBar")
+        self.tipsBar = self.statusTip()
         libTensor.loadGraph()
         self.setGeometry(100, 100, 800, 600)
-        self.statusBar()
         self.url = QLineEdit()
         self.toolbar.addWidget(QLabel("请输入图片地址或者选择本地图片:"))
         self.toolbar.addWidget(self.url)
@@ -79,6 +125,17 @@ class MainWindow(QMainWindow):
         self.openButton.setIcon(newIcon('file'))
         self.openButton.clicked.connect(self.open)
         self.toolbar.addWidget(self.openButton)
+        self.canvas = Canvas()
+        scroll = QScrollArea()
+        scroll.setWidget(self.canvas)
+        scroll.setWidgetResizable(True)
+        self.scrollBars = {
+            Qt.Vertical: scroll.verticalScrollBar(),
+            Qt.Horizontal: scroll.horizontalScrollBar()
+        }
+        self.canvas.scrollRequest.connect(self.scrollRequest)
+        self.setCentralWidget(scroll)
+        self.showTips("请输入图片网络地址或者选择本地图片")
 
 
 def main(argv=[]):
